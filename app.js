ba91fe03abc6ef805a6fe3e1c32e40f34ec49809
James Fox <james@gamma.app>
2023-04-22 10:40:22 -0700

    try staging test doc

--- app.js
+++ app.js
@@ -5,9 +5,22 @@ const https = require('https');
 const proxy = require('express-http-proxy');
 const port = process.env.PORT || 3001;
 
-const app = express();
-const basePath = "https://gamma-ahig3gpbv-gamma-app.vercel.app"
 
+// TEST DOC https://gamma-cdx8ch7ft-gamma-app.vercel.app/published/df3lyebptj85svu
+const basePath = "https://gamma-cdx8ch7ft-gamma-app.vercel.app";
+const publishedDocPath = "/published/df3lyebptj85svu";
+
+// https://gamma-ahig3gpbv-gamma-app.vercel.app/published/srdcvjfw933pv12
+// https://gamma.app/published/srdcvjfw933pv12
+// const basePath = "https://gamma-ahig3gpbv-gamma-app.vercel.app"
+// const publishedDocPath = "/published/srdcvjfw933pv12";
+
+
+
+
+
+
+const app = express();
 app.set('etag', false)
 app.use((req, res, next) => {
   res.setHeader('Surrogate-Control', 'no-store');
@@ -18,9 +31,6 @@ app.use((req, res, next) => {
 })
 
 
-// https://gamma-ahig3gpbv-gamma-app.vercel.app/published/srdcvjfw933pv12
-// https://gamma.app/embed/Your-B2B-Leads-Dont-Reply-Now-What-srdcvjfw933pv12
-
 const resourceProxy = proxy(basePath, {
   memoizeHost: false,
   proxyReqPathResolver: req => {
@@ -30,7 +40,7 @@ const resourceProxy = proxy(basePath, {
 
 const baseProxy = proxy(basePath, {
   memoizeHost: false,
-  proxyReqPathResolver: req => "/published/srdcvjfw933pv12",
+  proxyReqPathResolver: req => publishedDocPath,
   // userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
   //   console.log('Inserting base tag')
   //   const $ = cheerio.load(proxyResData.toString('utf8'));
