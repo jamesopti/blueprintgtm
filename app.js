3b605ea20c56167b1854e85f2c79f1e0c377e9b4
James Fox <james@gamma.app>
2023-04-21 19:33:26 -0700

    handle injecting

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
+const cheerio = require('cheerio');
 const express = require("express");
-const app = express();
+const proxy = require('express-http-proxy');
 const port = process.env.PORT || 3001;
 
+const app = express();
 const basePath = "https://gamma-ahig3gpbv-gamma-app.vercel.app"
 
-const url = require('url');
-const proxy = require('express-http-proxy');
+// https://gamma-ahig3gpbv-gamma-app.vercel.app/published/srdcvjfw933pv12
+// https://gamma.app/embed/Your-B2B-Leads-Dont-Reply-Now-What-srdcvjfw933pv12
 
 const resourceProxy = proxy(basePath, {
-    proxyReqPathResolver: req => url.parse(req.baseUrl).path
+  memoizeHost: false,
+  proxyReqPathResolver: req => {
+    return req.url
+  }
 });
 
 const baseProxy = proxy(basePath, {
-  proxyReqPathResolver: req => "/published/srdcvjfw933pv12"
+  memoizeHost: false,
+  proxyReqPathResolver: req => "/published/srdcvjfw933pv12",
+  proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
+    // you can update headers
+    // proxyReqOpts.headers['Content-Type'] = 'text/html';
+    // proxyReqOpts.headers['upgrade-insecure-requests'] = '0';
+    console.log(proxyReqOpts.headers)
+    return proxyReqOpts;
+  },
+  userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+    const $ = cheerio.load(proxyResData.toString('utf8'));
+    console.log($.html());
+    $('head').prepend(`<base href="${basePath}">`);
+    return $.html();
+  }
 });
 
 app.get('/', baseProxy);
